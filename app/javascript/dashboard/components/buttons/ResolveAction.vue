4b11432d1cda1e8f3c5eb7f358aeb0c9fdedab66
bimaste <abimaeltedesco@gmail.com>
2025-06-17 23:02:18 -0300

    Hide save button until label added

--- app/javascript/dashboard/components/buttons/ResolveAction.vue
+++ app/javascript/dashboard/components/buttons/ResolveAction.vue
@@ -95,12 +95,17 @@ const toggleStatus = (status, snoozedUntil) => {
 };
 
 const onSaveResolutionNote = async note => {
+  if (!conversationLabels.value.length) {
+    useAlert(t('CONVERSATION.LABEL_REQUIRED_TO_RESOLVE'));
+    return;
+  }
   noteText = note;
-  if (!noteText) return;
-  await store.dispatch('contactNotes/create', {
-    contactId: currentChat.value.meta.sender.id,
-    content: noteText,
-  });
+  if (noteText) {
+    await store.dispatch('contactNotes/create', {
+      contactId: currentChat.value.meta.sender.id,
+      content: noteText,
+    });
+  }
   toggleStatus(wootConstants.STATUS_TYPE.RESOLVED);
 };
 
@@ -112,6 +117,14 @@ const onCloseResolutionNote = () => {
 const onCmdOpenConversation = () => {
   toggleStatus(wootConstants.STATUS_TYPE.OPEN);
 };
+
+const conversationLabels = computed(() => {
+  return (
+    store.getters['conversationLabels/getConversationLabels'](
+      currentChat.value.id
+    ) || []
+  );
+});
 
 const onCmdResolveConversation = () => {
   showResolutionNoteModal.value = true;
